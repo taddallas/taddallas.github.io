931e3641b07f0a3cf260671fbfa3cbb83598f321
taddallas <tdallas@ucdavis.edu>
2019-01-02 16:45:19 +0200

    update papers and index

---
+++ resources/CV/yamlParse.py
@@ -0,0 +1,16 @@
+import yaml
+import sys
+
+with open('papers.yml', 'r') as f:
+    doc = yaml.load(f)
+
+pubs = open(pubs+".tex", "w")
+
+for i in range(len(doc))
+  newPost.write("r " + code + " words " + code + " words ")
+
+
+
+
+
+newPost.close()
